f8c9f522ebc3236fc8d88f02ef837c001df2afab
DESKTOP-M46KT46\FilipPliński4TI <filip06plinski@Gmail.com>
2024-09-30 11:12:24 +0200

    30.09

---
+++ filmy/src/App.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App{
+  return{
+    <div className="App">
+    <form>
+      <div class="mb-3">
+        <div id="film" class="form-label">Tytuł filmy</div>
+        <input type="text" class="form-control" id="film"/>
+  </div>
+  <div id="Wybierz" class="from-label">Wybierz rodzaj filmu</div>
+  <select id="select" class="form-control">
+    <option></option>
+    <option>Komedia</option>
+    <option>Sensacyjny</option>
+    <option>Horror</option>
+    <option>Obyczajowy</option>
+      </select>
+    <div><button type="submit" class="btn btn-primary">Dodaj</button>
+        </form>
+      </div>
+  }
+}
+  
+
+  
+  
+
+export default App;
